480ab884769576cd5f3c7ecf61344ad29ef1941e
DevDoge <lhuthefranc@hotmail.com>
2022-06-05 15:35:02 -0300

    Collapsible Sidebar #1

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,13 +11,17 @@ import { NavComponent } from './components/template/nav/nav.component';
 
 import { MatSidenavModule } from'@angular/material/sidenav';
 import { MatListModule } from'@angular/material/list';
+import { HomeComponent } from './components/views/home/home.component';
+import { DashboardComponent } from './components/views/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
