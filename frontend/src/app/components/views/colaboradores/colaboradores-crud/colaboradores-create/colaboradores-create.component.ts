04d38b33f6cb1742321bee7862918ed7c958b0b0
DevDoge <lhuthefranc@hotmail.com>
2022-06-14 17:49:28 -0300

    Colaboradores backend update

--- frontend/src/app/components/views/colaboradores/colaboradores-crud/colaboradores-create/colaboradores-create.component.ts
+++ frontend/src/app/components/views/colaboradores/colaboradores-crud/colaboradores-create/colaboradores-create.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import {Colaborador} from "../colaboraores.module";
 
 @Component({
   selector: 'app-colaboradores-create',
   templateUrl: './colaboradores-create.component.html',
-  styleUrls: ['./colaboradores-create.component.css']
+  styleUrls: ['./colaboradores-create.component.scss']
 })
 export class ColaboradoresCreateComponent implements OnInit {
+
+  colaborador: Colaborador = {
+    cpf: null,
+    matricula: null,
+    nome: "",
+    sobrenome: "",
+    email: "",
+    telefone: null,
+    cargo: "",
+    empresa: "",
+    supervisor: "",
+    perfil: "",
+    permissao: ""
+  }
 
   constructor() { }
 
---
+++ frontend/src/app/components/views/colaboradores/colaboradores-crud/colaboraores.module.ts
@@ -0,0 +1,14 @@
+export interface Colaborador{
+    id?: number;
+    cpf: number;
+    matricula: number;
+    nome: string;
+    sobrenome: string;
+    email: string;
+    telefone: number;
+    cargo: string;
+    empresa: string;
+    supervisor: string;
+    perfil: string;
+    permissao: string;
+}
